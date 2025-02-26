d1a0049d1a3045e9c7aaf581f31b442b247a5717
Antonio Moscato <antonio.moscato@runelab.it>
2025-02-26 10:03:23 +0100

    fix(config): fix urlBack

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -6,7 +6,7 @@ console.error(
   'Non funziona la lettura da .env, prima di pubblicare su render risolvere'
 );
 const baseUrl: string =
-  process.env.REACT_APP_BASE_URL || 'http://localhost:9000';
-// process.env.REACT_APP_BASE_URL || "https://gestionale-latest.onrender.com"
+  // process.env.REACT_APP_BASE_URL || 'http://localhost:9000';
+  process.env.REACT_APP_BASE_URL || 'https://gestionale-latest.onrender.com';
 
 export { baseUrl };
