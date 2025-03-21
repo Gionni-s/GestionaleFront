ce12a18de972794153a05d114863b0c31eeeca15
Antonio Moscato <antonio.moscato@runelab.it>
2025-03-21 14:28:30 +0100

    fix(core): fix layout

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -2,7 +2,7 @@ import Screen from './components';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <main className="flex flex-col items-center justify-between p-10">
       <Screen />
     </main>
   );
