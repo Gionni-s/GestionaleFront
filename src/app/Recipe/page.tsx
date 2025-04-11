c5994f19b289e4cf76f502fa16eb5d8fe5218873
Antonio Moscato <antonio.moscato@runelab.it>
2025-04-11 11:42:32 +0200

    feat(pages): normalize pagination showing

--- src/app/Recipe/page.tsx
+++ src/app/Recipe/page.tsx
@@ -2,7 +2,7 @@ import Screen from './components';
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-between p-10">
+    <main className="flex flex-col items-center justify-between p-5">
       <Screen />
     </main>
   );
